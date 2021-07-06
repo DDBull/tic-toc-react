0b33758eea77541e5d91b9de1e45e7294b3cc1a6
Dauren Dyussupov <dyussupov.d@gmail.com>
2021-07-06 17:52:57 +0500

    detecting winner

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Square = (props) => {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
+const Square = (props) => (
+  <button className="square" onClick={props.onClick}>
+    {props.value}
+  </button>
+);
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
-  
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -23,7 +41,7 @@ class Board extends React.Component {
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      squares: squares,
+      squares,
       xIsNext: !this.state.xIsNext,
     });
   }
@@ -38,7 +56,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -62,7 +86,7 @@ class Board extends React.Component {
     );
   }
 }
-  
+
 class Game extends React.Component {
   render() {
     return (
@@ -78,11 +102,10 @@ class Game extends React.Component {
     );
   }
 }
-  
+
 // ========================================
-  
+
 ReactDOM.render(
   <Game />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-  
